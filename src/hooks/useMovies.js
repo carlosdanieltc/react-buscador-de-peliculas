74f362acc74682a87059cb7cf0593119b3036a9e
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-16 13:26:19 +0200

    Buscador de Películas curso Midudev

---
+++ src/hooks/useMovies.js
@@ -0,0 +1,42 @@
+import { useRef, useState, useMemo, useCallback } from 'react'
+import { searchMovies } from '../services/movies.js'
+
+export function useMovies({ search, sort }) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const previousSearch = useRef(search)
+
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return
+
+    try {
+      setLoading(true)
+      setError(null)
+
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  // const sortedMoviesByYear = useMemo(() => {
+  //   if (movies == null) return
+  //   return sort 
+  //     ? [...movies].sort((a,b) => b.year - a.year)
+  //     : movies
+  // }, [sort, movies])
+
+  const sortedMoviesByTitle = useMemo(() => {
+    if (movies == null) return
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+  return { movies: sortedMoviesByTitle, getMovies, loading }
+}
